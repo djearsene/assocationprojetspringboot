32dd04a473647606722d7721b9a62a963a0ab612
DjeArsene <ouanyou21@gmail.com>
2025-07-28 11:09:33 +0200

    communiquer avec la base de donnée

---
+++ src/main/java/com/asso/repository/EmployeeRepository.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.asso.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import comm.asso.model
+
+@Repository
+/**
+ * 
+ */
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
