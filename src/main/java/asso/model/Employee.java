a71e7cf110ec3fdf349faa2efd6accc29b9f0167
= <brochettes40@outlook.fr>
2025-11-29 15:49:59 +0100

    First unit tets created

--- src/main/java/asso/model/Employee.java
+++ src/main/java/asso/model/Employee.java
@@ -6,9 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "employees")
 public class Employee {
